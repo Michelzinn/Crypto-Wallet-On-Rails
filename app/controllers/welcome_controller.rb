1d7ad2481bb65c49fefb875b27c3bb7ec91b6867
Michelzinn <michelbreno5@hotmail.com>
2022-06-09 10:07:03 -0300

    aaa

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:curso] = "Curso de Ruby on Rails (sou um cookie)"
+    session[:curso] = "Curso de Ruby on Rails (sou uma session)"
     @nome = params[:nome]
     @curso = params[:curso]
     
