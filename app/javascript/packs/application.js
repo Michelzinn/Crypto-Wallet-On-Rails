1305a20ffa7c69951c6b6eb6080fd81f8002773e
Michelzinn <michelbreno5@hotmail.com>
2022-06-15 14:36:31 -0300

    fix_

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,9 +10,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-require ("jquery")
-require ("bootstrap")
-
+import 'bootstrap'
 
 
 
